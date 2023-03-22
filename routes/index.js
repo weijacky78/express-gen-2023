0810f5820a85e405f5185969a01a32e74ebb0cdd
Xin <weijacky78@hotmail.com>
2023-03-22 10:10:59 -0400

    expressjs lab

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'ExpressJS Lab2 -- Xin' });
+});
+
+router.get('/:msg', function (req, res, next) {
+  res.render('index', { title: req.params.msg });
 });
 
 module.exports = router;
